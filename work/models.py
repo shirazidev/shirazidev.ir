6b7e02bd9e120fbbd3698368832ada7de294924d
AmirHossein Shirazi <139399374+shirazidev@users.noreply.github.com>
2023-12-10 11:11:40 +0330

    adding customizable primary key to workapp

--- work/models.py
+++ work/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Work(models.Model):
+    id = models.IntegerField(primary_key=True)
     tarikh = models.CharField(max_length=150)
     title = models.CharField(max_length=150)
     desc = models.TextField(max_length=500)
