c62413dbacc7596fba945b54eb03f36ae1c448ec
AmirHossein Shirazi <ashzih2@gmail.com>
2023-12-24 07:55:18 +0330

    add export and import to django admin

--- shirazidev_project/settings.py
+++ shirazidev_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g0mmz0e$c+sxi#v4ikd1)gu)8l(%z+$ss)#%y(63w=d0h=(vl='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://shirazidev.ir']
+# ALLOWED_HOSTS = ['https://shirazidev.ir']
 
 # Application definition
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
     #extention apps
     'django_cleanup.apps.CleanupConfig',
+    'import_export'
 ]
 
 MIDDLEWARE = [
